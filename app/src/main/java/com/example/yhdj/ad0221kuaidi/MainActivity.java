bd2c58b2f400d1d6953ef26eb63ed459d163071f
yhdj <1079652907@qq.com>
2017-02-21 04:49:28 +0800

    第三次提交-MainActivity

--- app/src/main/java/com/example/yhdj/ad0221kuaidi/MainActivity.java
+++ app/src/main/java/com/example/yhdj/ad0221kuaidi/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.yhdj.ad0221kuaidi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edtCom;
+    private EditText edtNo;
+    private Button btnCom;
+    private  Button btnDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        edtCom = (EditText) findViewById(R.id.edt_com);
+        edtNo = (EditText) findViewById(R.id.edt_no);
+        btnCom = (Button) findViewById(R.id.btn_com);
+        btnDetail = (Button) findViewById(R.id.btn_detail);
+
+        btnCom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
